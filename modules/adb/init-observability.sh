62d3bef9e37a3d27695c41de3558e70d566e1361
Alexandre Gattiker <algattik@microsoft.com>
2023-04-24 12:23:33 +0200

    .

--- modules/adb/init-observability.sh
+++ modules/adb/init-observability.sh
@@ -3,15 +3,18 @@
 set -euxo pipefail  # stop on error
 
 if [[ ${DB_IS_DRIVER:-TRUE} = "TRUE" ]]; then
-  # Driver JMX metrics are named e.g. `databricks.0.ExecutorMetrics.DirectPoolMemory`
+  # Driver JMX metrics are named e.g. `databricks.driver.ExecutorMetrics.DirectPoolMemory`
   DB_NODE_TYPE="driver"
+  DB_NODE_TYPE="databricks.driver"
 else
   # Executor JMX metrics are named e.g. `databricks.0.ExecutorMetrics.DirectPoolMemory`
   # Note that Databricks always runs one executor per worker node.
-  DB_NODE_TYPE="0"
+  DB_NODE_TYPE="worker"
+  DB_METRICS_PREFIX="databricks.0"
 fi
 
 export DB_NODE_TYPE
+export DB_METRICS_PREFIX
 
 echo "Listing /dbfs/observability"
 find /dbfs/observability
