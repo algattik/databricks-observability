6c155fdfbbe6eb52f1c9a490519353cdcb2d33f4
Alexandre Gattiker <algattik@microsoft.com>
2023-05-07 07:15:55 +0200

    Update main.tf

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -6,14 +6,14 @@ resource "azurerm_key_vault" "adb_kv" {
   resource_group_name        = var.resource_group_name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
-  purge_protection_enabled   = true
+  purge_protection_enabled   = false
   sku_name                   = "standard"
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     secret_permissions = [
-      "Get", "List", "Set"
+      "Get", "List", "Set", "Delete", "Recover", "Purge"
     ]
   }
 }
