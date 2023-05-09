44c996e94f332fb5ea297b8443e79f0098799226
Alexandre Gattiker <algattik@microsoft.com>
2023-05-09 15:13:14 +0200

    .

---
+++ modules/databricks/notebooks/sample-telemetry-notebook.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# exports notebook_name, tracer, logger, meter
+
+# COMMAND ----------
+
+# MAGIC %run ./telemetry-helper
+
+# COMMAND ----------
+
+trips_metric = meter.create_histogram(
+    name="trips", description="Number of trips"
+)
+
+with tracer.start_as_current_span("process trips"):
+
+    with tracer.start_as_current_span("write trips table"):
+
+        logger.critical("Saving data to table %s", "trips2")
+
+        (spark.table("samples.nyctaxi.trips")
+        .write
+        .mode("overwrite")
+        .option("path", spark.conf.get("storage_uri") + "/trips2")
+        .saveAsTable("trips2"))
+
+    with tracer.start_as_current_span("count trips table"):
+
+        trips_saved = spark.table("trips2").count()
+
+        trips_metric.record(trips_saved)
+
+    logger.critical("%d trips completed", trips_saved)
+
