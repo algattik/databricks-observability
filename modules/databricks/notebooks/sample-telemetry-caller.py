579e8bdacc56b79c8b57c08104ac3c0523f2e8e4
Alexandre Gattiker <algattik@microsoft.com>
2023-05-11 04:02:46 +0200

    Revert "Python custom metrics (#7)"
    This reverts commit 1039793aff40db168905aab60f1ba056c65be11a.

---
+++ modules/databricks/notebooks/sample-telemetry-caller.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# azure-monitor-opentelemetry~=1.0.0b10 
+
+# COMMAND ----------
+
+# MAGIC %run ./telemetry-helper
+
+# COMMAND ----------
+
+run("sample-telemetry-notebook", timeout_seconds=0)
